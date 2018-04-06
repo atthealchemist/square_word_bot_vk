6810004f80901886ca7c68f0e401c1d70f27d14b
The Alchemist <at.thealchemist@gmail.com>
2018-04-06 18:39:09 +0300

    added vertical function

--- word.py
+++ word.py
@@ -30,3 +30,42 @@ def make_corner_word(word):
             corner_word += (word[row_index] + "\n")
 
     return corner_word
+
+def get_greatest_word_len(words):
+    raw_words = words.split(' ')
+    prev = 0
+    new = 0
+    for word in raw_words:
+        if len(word) > new:
+            prev = new
+            new = len(word)
+        else:
+            continue
+    return new
+
+
+def make_vertical_words(words):
+    raw_words = words.split(' ')
+    vertical_words = []
+    vertical_word = ""
+    greatest_word_len = get_greatest_word_len(words)
+    position_in_array = 0
+    while len(vertical_words) < greatest_word_len:
+        for raw_word in raw_words:
+            try:
+                for letter_index, letter in enumerate(raw_word[position_in_array]):
+                    vertical_word += raw_word[position_in_array] + " "
+            except Exception:
+                space = " "
+                vertical_word += space + " "
+                continue
+        vertical_words.append(vertical_word)
+        if position_in_array < greatest_word_len - 1:
+            position_in_array += 1
+        vertical_word = ""
+    return vertical_words
+
+# if __name__ == '__main__':
+#     for word in make_vertical_words("тюльпашки для ромашки"):
+#         print(word + "\n")
+    #print(make_vertical_words("ремонт обуви_ копир_ ключей"))
--- word_bot_vk.py
+++ word_bot_vk.py
@@ -1,23 +1,26 @@
 import sys
 import time
 import vk_api
-from word import make_square_word, make_corner_word
+from word import make_square_word, make_corner_word, make_vertical_words
 
-vk = vk_api.VkApi(login = sys.argv[1], password = sys.argv[2])
+vk = vk_api.VkApi(login=sys.argv[1], password=sys.argv[2])
 vk.auth()
 send_type = sys.argv[3]
 get_id = sys.argv[4]
 username = ""
 if(send_type == "user"):
-    values = {'out': 0,'count': 1,'time_offset': 60, 'peer_id': int(get_id)}
+    values = {'out': 0, 'count': 1, 'time_offset': 60, 'peer_id': int(get_id)}
     user = vk.method('users.get', {'user_ids': get_id})[0]
-    username = user['first_name'] + " " + user['last_name'];
-    print("Bot is started (for user \"{0}\" (id: {1})) !".format(username, get_id))
+    username = user['first_name'] + " " + user['last_name']
+    print("Bot is started (for user \"{0}\" (id: {1})) !".format(
+        username, get_id))
 else:
-    values = {'out': 0,'count': 1,'time_offset': 60, 'peer_id': 2000000000 + int(get_id)}
+    values = {'out': 0, 'count': 1, 'time_offset': 60,
+              'peer_id': 2000000000 + int(get_id)}
     chat = vk.method('messages.getChat', {'chat_id': get_id})
     chat_title = chat['title']
-    print("Bot is started (for chat \"{0}\" (chat_id: {1}))!".format(chat_title, get_id))
+    print("Bot is started (for chat \"{0}\" (chat_id: {1}))!".format(
+        chat_title, get_id))
 
 
 while True:
@@ -25,41 +28,59 @@ while True:
     if response['items']:
         values['last_message_id'] = response['items'][0]['id']
     for item in response['items']:
-            user_id = item['user_id']
-            word = item['body']
-            if("сделай квадрат " in word):
-                # print("Message: {0}".format(word))
-                word = word.split(' ')
-                w = word[-1]
-                if(send_type == "user"):
-                    vk.method('messages.send', {'peer_id': get_id, 'message': make_square_word(w)})
-                else:
-                    vk.method('messages.send', {'peer_id': 2000000000 + int(get_id), 'message': make_square_word(w)})
-            elif("сделай квадраты" in word):
-                # print("Making more than 1 square")
-                words = word.split(" ")
-                cmd_end_index = words.index("квадраты") + 1
-                for w in words[cmd_end_index:len(words)]:
-                    if(send_type == "user"):
-                        vk.method('messages.send', {'peer_id': get_id, 'message': make_square_word(w)})
-                    else:
-                        vk.method('messages.send', {'peer_id': 2000000000 + int(get_id), 'message': make_square_word(w)})
-            elif("сделай угол " in word):
-                word = word.split(' ')
-                w = word[-1]
-                if(send_type == "user"):
-                    vk.method('messages.send', {'peer_id': get_id, 'message': make_corner_word(w)})
-                else:
-                    vk.method('messages.send', {'peer_id': 2000000000 + int(get_id), 'message': make_corner_word(w)})
-            elif("сделай углы" in word):
-                words = word.split(" ")
-                cmd_end_index = words.index("углы") + 1
-                for w in words[cmd_end_index:len(words)]:
-                    if(send_type == "user"):
-                        vk.method('messages.send', {'peer_id': get_id, 'message': make_corner_word(w)})
-                    else:
-                        vk.method('messages.send', {'peer_id': 2000000000 + int(get_id), 'message': make_corner_word(w)})
+        user_id = item['user_id']
+        word = item['body']
+        if("сделай квадрат " in word):
+            # print("Message: {0}".format(word))
+            word = word.split(' ')
+            w = word[-1]
+            if(send_type == "user"):
+                vk.method('messages.send', {
+                          'peer_id': get_id, 'message': make_square_word(w)})
             else:
-                pass
+                vk.method('messages.send', {
+                          'peer_id': 2000000000 + int(get_id), 'message': make_square_word(w)})
+        elif("сделай квадраты" in word):
+            # print("Making more than 1 square")
+            words = word.split(" ")
+            cmd_end_index = words.index("квадраты") + 1
+            for w in words[cmd_end_index:len(words)]:
+                if(send_type == "user"):
+                    vk.method('messages.send', {
+                              'peer_id': get_id, 'message': make_square_word(w)})
+                else:
+                    vk.method('messages.send', {
+                              'peer_id': 2000000000 + int(get_id), 'message': make_square_word(w)})
+        elif("сделай угол " in word):
+            word = word.split(' ')
+            w = word[-1]
+            if(send_type == "user"):
+                vk.method('messages.send', {
+                          'peer_id': get_id, 'message': make_corner_word(w)})
+            else:
+                vk.method('messages.send', {
+                          'peer_id': 2000000000 + int(get_id), 'message': make_corner_word(w)})
+        elif("сделай углы" in word):
+            words = word.split(" ")
+            cmd_end_index = words.index("углы") + 1
+            for w in words[cmd_end_index:len(words)]:
+                if(send_type == "user"):
+                    vk.method('messages.send', {
+                              'peer_id': get_id, 'message': make_corner_word(w)})
+                else:
+                    vk.method('messages.send', {
+                              'peer_id': 2000000000 + int(get_id), 'message': make_corner_word(w)})
+        elif("сделай вертикаль" in word):
+            words = word.split(" ")
+            cmd_end_index = words.index("вертикаль") + 1
+            for word in words[cmd_end_index:len(words)]):
+                if(send_type == "user"):
+                    print(" ".join(words[cmd_end_index:len(words)]))
+                    vk.method('messages.send', {
+                                'peer_id': get_id, 'message': make_vertical_words(word)
+                else:
+                    vk.method('messages.send', {
+                                'peer_id': 2000000000 + int(get_id), 'message': make_vertical_words(word)
+        else:
+            pass
     time.sleep(3)
-    
