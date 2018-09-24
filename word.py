9491900482d263cc20175d8658c561605a2264c4
Alexey Konev <at.thealchemist@gmail.com>
2018-09-24 18:28:56 +0300

    added config, made squares

--- word.py
+++ word.py
@@ -1,71 +1,109 @@
-def MakeSquare(word):
-    squareWord = ""
-    if word != "":
-        word = list(word)
-        currentIndex = 0
-        while currentIndex < len(word):
+class WordFactory:
+
+    def __init__(self, config):
+        self.config = config
+
+    def item(self, item):
+        print("self.factory.item ", item)
+        for cmd in self.config['commands']:
+            for word in cmd['name']['single']:
+                if word in item:
+                    print("self.factory.item.single ", item)
+                    if word.strip() in ["квадрат", "square"]:
+                        print("--> квадрат")
+                        it = item.split(' ')[-1]
+                        print(self.square(it))
+                        return self.square(it)
+            for word in cmd['name']['multiple']:
+                if word in item:
+                    print("self.factory.item.multi ", item)
+                    if word.strip() in ["квадраты", "squares"]:
+                        print("--> квадраты")
+
+                        words = item.split(' ')
+                        endOfCommandIndex = words.index(word.strip()) + 1
+                        items = words[endOfCommandIndex:len(words)]
+                        print(self.squares(items))
+                        return self.squares(items)
+
+    def squares(self, words):
+        squares = []
+        for word in words:
+            squares.append(self.square(word))
+        return squares
+
+    def square(self, word):
+        squareWord = ""
+        if word != "":
+            word = list(word)
+            currentIndex = 0
+            while currentIndex < len(word):
+                for rowIndex, letter in enumerate(word):
+                    # print(word[row_index], end=" ")
+                    squareWord += (word[rowIndex] + " ")
+                firstLetter = word.pop(0)
+                word.append(firstLetter)
+                # print("")
+                squareWord += "\n"
+                currentIndex += 1
+        # print("")
+        squareWord += "\n"
+
+        return squareWord
+
+    def corner(self, word):
+        cornerWord = ""
+        if word != "":
+            word = list(word)
             for rowIndex, letter in enumerate(word):
-                # print(word[row_index], end=" ")
-                squareWord += (word[rowIndex] + " ")
-            firstLetter = word.pop(0)
-            word.append(firstLetter)
-            # print("")
-            squareWord += "\n"
-            currentIndex += 1
-    # print("")
-    squareWord += "\n"
+                if(rowIndex < len(word)):
+                    cornerWord += (word[rowIndex] + " ")
+            cornerWord += "\n"
+            word.pop(0)
+            for rowIndex, letter in enumerate(word):
+                cornerWord += (word[rowIndex] + "\n")
 
-    return squareWord
+        return cornerWord
 
-def MakeCorner(word):
-    cornerWord = ""
-    if word != "":
-        word = list(word)
-        for rowIndex, letter in enumerate(word):
-            if(rowIndex < len(word)):
-                cornerWord += (word[rowIndex] + " ")
-        cornerWord += "\n"
-        word.pop(0)
-        for rowIndex, letter in enumerate(word):
-            cornerWord += (word[rowIndex] + "\n")
-
-    return cornerWord
-
-def __getGreatestWordLength(words):
-    rawWords = words.split(' ')
-    prev = 0
-    new = 0
-    for word in rawWords:
-        if len(word) > new:
-            prev = new
-            new = len(word)
-        else:
-            continue
-    return new
-
-
-def MakeVerticals(words):
-    rawWords = words.split(' ')
-    verticalWords = []
-    verticalWord = ""
-    greatestWordLength = __getGreatestWordLength(words)
-    positionInArray = 0
-    while len(verticalWords) < greatestWordLength:
-        for rawWord in rawWords:
-            try:
-                for letter_index, letter in enumerate(rawWord[positionInArray]):
-                    verticalWord += rawWord[positionInArray] + " "
-            except Exception:
-                space = " "
-                verticalWord += space + " "
-                continue
-        verticalWords.append(verticalWord)
-        if positionInArray < greatestWordLength - 1:
-            positionInArray += 1
+    def corners(self, words):
+        corners = []
+        for word in words:
+            corners.append(self.corner(word))
+        return corners
+    
+    def verticals(self, words):
+        rawWords = words.split(' ')
+        verticalWords = []
         verticalWord = ""
-    return verticalWords
+        greatestWordLength = self.__getGreatestWordLength(words)
+        positionInArray = 0
+        while len(verticalWords) < greatestWordLength:
+            for rawWord in rawWords:
+                try:
+                    for letter_index, letter in enumerate(rawWord[positionInArray]):
+                        verticalWord += rawWord[positionInArray] + " "
+                except Exception:
+                    space = " "
+                    verticalWord += space + " "
+                    continue
+            verticalWords.append(verticalWord)
+            if positionInArray < greatestWordLength - 1:
+                positionInArray += 1
+            verticalWord = ""
+        return verticalWords
 
-if __name__ == '__main__':
-    for word in MakeVerticals("тюльпашки для ромашки"):
-        print(word + "\n")
-    print(MakeVerticals("ремонт обуви копир ключей"))
+    def __getGreatestWordLength(self, words):
+        rawWords = words.split(' ')
+        prev = 0
+        new = 0
+        for word in rawWords:
+            if len(word) > new:
+                prev = new
+                new = len(word)
+            else:
+                continue
+        return new
+
+# if __name__ == '__main__':
+#     factory = WordFactory()
+#     print(factory.verticals("ремонт обуви копир ключей"))
--- word_bot_vk.py
+++ word_bot_vk.py
@@ -3,67 +3,60 @@ import os
 import time
 import json
 
-from vk_api import VkApi
-from word import MakeSquare, MakeCorner, MakeVerticals
+from vk_api import VkApi, exceptions
+from word import WordFactory
 
 
 class WordBot:
+
     config = {}
+    commands = []
 
-    commands = [
-        {
-            'type': 'command',
-            'function': MakeSquare,
-            'single': 'квадрат',
-            'multiple': 'квадраты',
-        },
-         {
-            'type': 'command',
-            'function': MakeCorner,
-            'single': 'угол',
-            'multiple': 'углы',
-        },
-         {
-            'type': 'command',
-            'function': MakeVerticals,
-            'single': 'вертикаль',
-            'multiple': 0,
-        },
-    ]
+    def __getUser(self):
+        return self.vk.method('users.get', {'user_ids': self.userId})[0]
 
-    def execute(self, command):
-        locals()[command]()
+    def __getChat(self):
+        return self.vk.method('messages.getChat', {'chat_id': self.chatId})
+
+    def __sendMessage(self, text):
+        self.vk.method('messages.send', {
+                       'peer_id': self.getPeerId(), 'message': text})
 
     def parseCommand(self, message):
-        if "сделай квадрат " in message:
-            # print("Message: {0}".format(word))
-            word = message.split(' ')
-            arg = word[-1]
-            self.vk.method('messages.send', {'peer_id': self.getPeerId(), 'message': MakeSquare(arg)})
-        elif "сделай квадраты" in message:
-            # print("Making more than 1 square")
-            words = message.split(" ")
-            cmd_end_index = words.index("квадраты") + 1
-            for w in words[cmd_end_index:len(words)]:
-                self.vk.method('messages.send', {'peer_id': self.getPeerId(), 'message': MakeSquare(w)})
-        elif "сделай угол " in message:
-            word = message.split(' ')
-            w = word[-1]
-            self.vk.method('messages.send', {'peer_id': self.getPeerId(), 'message': MakeCorner(w)})
-        elif "сделай углы" in message:
-            words = message.split(" ")
-            cmd_end_index = words.index("углы") + 1
-            for w in words[cmd_end_index:len(words)]:
-                self.vk.method('messages.send', {'peer_id': self.getPeerId(), 'message': MakeCorner(w)})
-        elif "сделай вертикаль" in message:
-            words = message.split(" ")
-            cmd_end_index = words.index("вертикаль") + 1
-            for word in MakeVerticals(words[-1]):
-                self.vk.method('messages.send', {'peer_id': self.getPeerId(), 'message': word + '\n'})
-        else:
-            pass
+        
+        for cmd in self.commands:
+            if cmd['name']['single'] in message:
+                count = message.index(cmd['name']['single'])
+                words = message.split(' ')
+                item = self.factory.item(words[-1])
+
+                command = {
+                    'count': words[count],
+                    'raw': message,
+                    'text': words.join(' ')
+                }
+
+                print("bot.parseCommand.command.single", command)
+
+                self.__sendMessage(item)
+            elif cmd['name']['multiple'] in message:
+                count = message.index(cmd['name']['multiple'])
+                words = message.split(' ')
+                mult = words[count]
+
+                command = {
+                    'count': mult,
+                    'raw': message,
+                    'text': words[mult+1:len(words)]
+                }
+
+                print("bot.parseCommand.command.multi", command)
+
+                endOfCommandIndex = words.index(cmd['name']['miltiple']) + 1
+                items = words[endOfCommandIndex:len(words)]
+                result = self.factory.item(items)
+                self.__sendMessage(result)
     time.sleep(3)
-                
 
     def getPeerId(self):
         if self.config['settings']['mode'] == "chat":
@@ -73,151 +66,75 @@ class WordBot:
 
     def getDialogInfo(self):
         if self.config['settings']['mode'] == "user":
-            self.requestParams = {'out': 0, 'count': 1, 'time_offset': 60, 'peer_id': self.userId}
-            user = self.vk.method('users.get', {'user_ids': self.userId})[0]
+            self.requestParams = {'out': 0, 'count': 1,
+                                  'time_offset': 60, 'peer_id': self.userId}
+            user = self.__getUser()
             userName = user['first_name'] + " " + user['last_name']
             print("Bot is started (for user \"{0}\" (id: {1})) !".format(
                 userName, self.userId))
         else:
-            self.requestParams = {'out': 0, 'count': 1, 'time_offset': 60, 'peer_id': self.getPeerId()}
-            chat = self.vk.method('messages.getChat', {'chat_id': self.chatId})
+            self.requestParams = {'out': 0, 'count': 1,
+                                  'time_offset': 60, 'peer_id': self.getPeerId()}
+            chat = self.__getChat()
             chatTitle = chat['title']
             print("Bot is started (for chat \"{0}\" (chat_id: {1}))!".format(
                 chatTitle, self.chatId))
 
+    def loadCommands(self):
+        self.commands = self.__loadFromJsonFile("commands.json")
+
     def loadConfig(self):
-        with open(os.path.join(os.getcwd(), "config.json"), 'r', encoding='utf8') as jsonfile:
-            self.config = json.loads("".join(jsonfile.readlines()))
+        self.config = self.__loadFromJsonFile("config.json")
+
+    def __loadFromJsonFile(self, fileName):
+        with open(os.path.join(os.getcwd(), fileName), 'r', encoding='utf8') as jsonfile:
+            return json.loads("".join(jsonfile.readlines()))
 
     def getLatestMessageFromHistory(self):
         response = self.vk.method('messages.getHistory', self.requestParams)
         if response['items']:
             self.requestParams['last_message_id'] = response['items'][0]['id']
-        return response
+        return response['items'][0]['text']
 
     def __init__(self):
-        self.loadConfig()
 
-        loggedIn = self.authenticate(
-            self.config['credentials']['user'], self.config['credentials']['pwd'])
-        if self.config['settings']['mode'] == "chat":
-            self.chatId = input("Enter chat id (XX from &sel=cXX): ")
-            print("Your chat id: {0}".format(self.chatId))
-        else:
-            self.userId = input("Enter user id (XX from &sel=cXX): ")
+        self.loadConfig()
+        self.loadCommands()
+        self.factory = WordFactory(self.config)
+
+        loggedIn = self.authenticate(self.config)
 
         self.getDialogInfo()
 
         while loggedIn:
-            msg = self.getLatestMessageFromHistory()
-            msgText = msg['items'][0]['text']
-            self.parseCommand(msgText)
-
-    def authenticate(self, user, pwd):
+            try:
+                msg = self.getLatestMessageFromHistory()
+                self.parseCommand(msg)
+            except exceptions.ApiError:
+                pass
+    def authenticate(self, config):
         try:
-            self.vk = VkApi(login=user, password=pwd)
+            self.vk = VkApi(
+                login=config['credentials']['user'], password=config['credentials']['pwd'])
             self.vk.auth()
         except Exception as ex:
             print("Can't login to VK! Possible reason: {0}".format(ex))
             return False
         finally:
-            print("Logged in")
+            print("Logged in.")
+            if not config['settings']['id']:
+                if config['settings']['mode'] == "chat":
+                    self.chatId = input("Enter chat id (XX from &sel=cXX): ")
+                    print("Your chat id: {0}".format(self.chatId))
+                else:
+                    self.userId = input("Enter user id (XX from &sel=cXX): ")
+            else:
+                if config['settings']['mode'] == "chat":
+                    self.chatId = config['settings']['id']
+                else:
+                    self.userId = config['settings']['id']
             return True
 
 
 if __name__ == "__main__":
     bot = WordBot()
-
-'''
-    send_type = "chat"
-    get_id = sys.argv[4]
-    username = ""
-
-    response_body = "text"
-    user_id = int(get_id)
-    chat_id = 2000000000 + int(get_id)
-
-    login = input("Enter login: ")
-    pwd = input("Enter password: ")
-
-    if login and pwd:
-        authenticate(login, pwd)
-        print("Stored login and password in config.json")
-
-    if(send_type == "user"):
-        values = {'out': 0, 'count': 1, 'time_offset': 60, 'peer_id': user_id}
-        user = vk.method('users.get', {'user_ids': get_id})[0]
-        username = user['first_name'] + " " + user['last_name']
-        print("Bot is started (for user \"{0}\" (id: {1})) !".format(
-            username, get_id))
-    else:
-        values = {'out': 0, 'count': 1, 'time_offset': 60,
-                  'peer_id': chat_id}
-        chat = vk.method('messages.getChat', {'chat_id': get_id})
-        chat_title = chat['title']
-        print("Bot is started (for chat \"{0}\" (chat_id: {1}))!".format(
-            chat_title, get_id))
-
-    while True:
-        response = vk.method('messages.getHistory', values)
-        if response['items']:
-            values['last_message_id'] = response['items'][0]['id']
-        for item in response['items']:
-            #print("dd {0}".format(item))
-            # user_id = item['user_id']
-            word = item[response_body]
-            if("сделай квадрат " in word):
-                # print("Message: {0}".format(word))
-                word = word.split(' ')
-                w = word[-1]
-                if(send_type == "user"):
-                    vk.method('messages.send', {
-                        'peer_id': get_id, 'message': MakeSquare(w)})
-                else:
-                    vk.method('messages.send', {
-                        'peer_id': 2000000000 + int(get_id), 'message': MakeSquare(w)})
-            elif("сделай квадраты" in word):
-                # print("Making more than 1 square")
-                words = word.split(" ")
-                cmd_end_index = words.index("квадраты") + 1
-                for w in words[cmd_end_index:len(words)]:
-                    if(send_type == "user"):
-                        vk.method('messages.send', {
-                            'peer_id': get_id, 'message': MakeSquare(w)})
-                    else:
-                        vk.method('messages.send', {
-                            'peer_id': 2000000000 + int(get_id), 'message': MakeSquare(w)})
-            elif("сделай угол " in word):
-                word = word.split(' ')
-                w = word[-1]
-                if(send_type == "user"):
-                    vk.method('messages.send', {
-                        'peer_id': get_id, 'message': MakeCorner(w)})
-                else:
-                    vk.method('messages.send', {
-                        'peer_id': 2000000000 + int(get_id), 'message': MakeCorner(w)})
-            elif("сделай углы" in word):
-                words = word.split(" ")
-                cmd_end_index = words.index("углы") + 1
-                for w in words[cmd_end_index:len(words)]:
-                    if(send_type == "user"):
-                        vk.method('messages.send', {
-                            'peer_id': get_id, 'message': MakeCorner(w)})
-                    else:
-                        vk.method('messages.send', {
-                            'peer_id': 2000000000 + int(get_id), 'message': MakeCorner(w)})
-            elif("сделай вертикаль" in word):
-                words = word.split(" ")
-                cmd_end_index = words.index("вертикаль") + 1
-                for word in words[cmd_end_index:len(words)]:
-                    if(send_type == "user"):
-                        print(" ".join(words[cmd_end_index:len(words)]))
-                        vk.method('messages.send', {
-                            'peer_id': get_id, 'message': MakeVerticals(word)})
-                    else:
-                        vk.method('messages.send', {
-                            'peer_id': 2000000000 + int(get_id), 'message': MakeVerticals(word)})
-            else:
-                pass
-        time.sleep(3)
-'''
